912e341441e4f5c11417ba840b5bc6a6b1f50cd4
Ritesh <riteshsingh3628@gmail.com>
2024-09-10 07:23:10 +0530

    First commit created a server

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const PORT = 5000;
+
+app.listen()
+
+app.listen(PORT,()=>{
+    console.log("Srever running....");
+})
---
+++ mongo.js
@@ -0,0 +1,1 @@
+const DB_URI = "mongodb://localhost:27017/bookDirectory"
